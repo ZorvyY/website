3eef6726e18a84e206c7654070cd1043ac5c269e
Zoravur Singh <zoravur.s@gmail.com>
2018-12-20 21:28:16 -0500

    First deploy

---
+++ src/theme.js
@@ -0,0 +1,69 @@
+// const colors = {
+//   background: '#FFFFFF',
+//   backgroundDark: '#f0e6f6',
+
+//   primary: '#7c37ad',
+//   primaryLight: '#ae66df',
+//   primaryDark: '#4b007d',
+
+//   secondary: '#ff4081',
+//   secondaryLight: '#ff79b0',
+//   secondaryDark: '#c60055',
+// };
+
+// const colors = {
+//   background: '#FFFFFF',
+//   backgroundDark: '#f0e6f6',
+
+//   primary: '#7c37ad',
+//   primaryLight: '#ae66df',
+//   primaryDark: '#4b007d',
+
+//   secondary: '#ff4081',
+//   secondaryLight: '#ff79b0',
+//   secondaryDark: '#ff4081',
+// };
+
+
+// const colors = {
+//   background: '#F5DD90',
+//   backgroundDark: '#D3BF7C',
+
+//   primary: '#586BA4',
+//   primaryLight: '#6E85CC',
+//   primaryDark: '#324376',
+
+//   secondary: '#DB5E53',
+//   secondaryLight: '#F76C5E',
+//   secondaryDark: '#F68E5F',
+// };
+
+// const colors = {
+//   background: '#FDFFFC',
+//   backgroundDark: '#FDFFFC',
+
+//   primary: '011627',
+//   primaryLight: '#F71735',
+//   primaryDark: '#990D20',
+
+//   secondary: '#41EAD4',
+//   secondaryLight: '#FF9F1C',
+//   secondaryDark: '#D1800E',
+// };
+
+const colors = {
+  background: '#CEEAEB',
+  backgroundDark: '#A6D9DB',
+
+  primary: '#1D3557',
+  primaryLight: '#457B9D',
+  primaryDark: '#15263F',
+
+  secondary: '#E63946',
+  secondaryLight: '#F1939A',
+  secondaryDark: '#A81E27',
+};
+
+module.exports = {
+  colors,
+};
